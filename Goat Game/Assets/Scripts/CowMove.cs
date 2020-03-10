30e7adac7c90822b3fea2f3248c351b137797679
Alex D <30276530+Doomus@users.noreply.github.com>
2020-03-09 22:23:03 -0700

    initial commit

---
+++ Goat Game/Assets/Scripts/CowMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CowMove : MonoBehaviour
+{
+    public NavMeshAgent cowNav;
+    public GameObject target;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("I collided");
+        if (col.gameObject.GetComponent<PlayerMove>())
+        {
+            target = col.gameObject;
+            Debug.Log("Target Aquired");
+        }
+    }
+
+
+    void Update()
+    {
+        if (target != null)
+        {
+            cowNav.SetDestination(target.transform.position);
+        }
+    }
+}
---
+++ Goat Game/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMove : MonoBehaviour
+{
+    public LayerMask ClickableWorld;
+    public GameObject navMarker;
+    public GameObject navMarkerPrefab;
+
+    private NavMeshAgent PlayerNav;
+    public bool markerPlaced;
+
+    void Start()
+    {
+        PlayerNav = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(clickRay, out hitInfo, 100, ClickableWorld))
+            {
+                Vector3 navPoint = hitInfo.point;
+                PlayerNav.SetDestination(hitInfo.point);
+                if (markerPlaced)
+                {
+                    Destroy(navMarker);
+                }
+                navMarker = Instantiate(navMarkerPrefab) as GameObject;
+                navMarker.transform.position = navPoint;
+                markerPlaced = true;
+
+                
+            }
+        }
+    }
+}
