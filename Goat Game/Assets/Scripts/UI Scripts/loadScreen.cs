5821a577d8b0a08a63613da0a753ef63e6740f67
Dasha Moyer <dashamoyer@gmail.com>
2020-04-14 01:19:08 -0700

    PlayButton
    There is a spinning cow and the play button loads the next scene

---
+++ Goat Game/Assets/Scripts/UI Scripts/loadScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadScreen : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+    }
+}
